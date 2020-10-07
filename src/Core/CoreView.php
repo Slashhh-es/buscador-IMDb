c28f766cc190ce3890b48c9abd06f5b8bae37d15
Javier Glez <javierglezr@hotmail.es>
2020-10-07 17:52:54 +0200

    ﻿Añadir una plantilla de Bootstrap

--- src/Core/CoreView.php
+++ src/Core/CoreView.php
@@ -19,13 +19,51 @@ class CoreView
         <!doctype html>
         <html lang="es">
             <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <meta name="description" content="">
+                <meta name="author" content="">
+
                 <title>Busca tu peli</title>
+
+                <link rel="stylesheet" href="./assets/css/main.css">
             </head>
 
             <body>
-                
-                '.$this->content.'
-                    
+                <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+                    <h5 class="my-0 mr-md-auto font-weight-normal">Busca tu peli</h5>
+                </div>
+
+                <div class="container px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                    <h1 class="display-4">Peliculas</h1>
+                    <form method="post">
+                        <div class="form-row">
+                            <div class="col-10">
+                                <label class="sr-only" for="inlineFormInput">Name</label>
+                                <input type="text" name="search" class="form-control mb-2" placeholder="Buscar peli">
+                            </div>
+                            <div class="col-2">
+                                <button type="submit" class="btn btn-primary btn-block mb-2">Buscar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="container">
+                    <div>
+                        '.$this->content.'
+                    </div>
+
+                    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+                        <div class="row">
+                            <div class="col-12">
+                                <small class="d-block mb-3 text-muted">&copy; 2020 Javier Glez</small>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+
+                <script src="./assets/js/main.js"></script>
             </body>
         </html>';
     }
